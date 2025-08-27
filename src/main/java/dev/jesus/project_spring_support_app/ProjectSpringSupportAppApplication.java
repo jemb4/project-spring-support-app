a221c3e7d0ec6840d50c7a83c7a5aa4fe8a466eb
Jesus Martin <jesus8.mb@gmail.com>
2025-08-27 12:12:00 +0200

    first commit

---
+++ src/main/java/dev/jesus/project_spring_support_app/ProjectSpringSupportAppApplication.java
@@ -0,0 +1,13 @@
+package dev.jesus.project_spring_support_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringSupportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringSupportAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jesus/project_spring_support_app/ProjectSpringSupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jesus.project_spring_support_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringSupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
