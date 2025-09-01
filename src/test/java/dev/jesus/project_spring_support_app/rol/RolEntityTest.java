b09a969f17d1a38ad39199de7230c8aa850b7a78
Jesus Martin <jesus8.mb@gmail.com>
2025-09-01 23:21:40 +0200

    test: testing rolentity

---
+++ src/test/java/dev/jesus/project_spring_support_app/rol/RolEntityTest.java
@@ -0,0 +1,35 @@
+package dev.jesus.project_spring_support_app.rol;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RolEntityTest {
+
+  private RolEntity rol;
+
+  @BeforeEach
+  void setUp() {
+    rol = new RolEntity(1L, "user");
+  }
+
+  @Test
+  void testRolEntity_InitializationWithIdAndName() {
+
+    assertThat(rol, is(instanceOf(RolEntity.class)));
+    assertThat(rol.getClass().getDeclaredFields().length, is(equalTo(2)));
+    assertThat(rol.getName(), is(equalTo("user")));
+  }
+
+  @Test
+  void testRolEntity() {
+    rol.setId(2L);
+    rol.setName("user2");
+    assertThat(rol.getId(), is(equalTo(2L)));
+    assertThat(rol.getName(), is(equalTo("user2")));
+  }
+}
