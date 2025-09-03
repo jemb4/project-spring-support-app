52e7f1e6a6aa89712a4b821b1da9972d805fa86f
Jesus Martin <jesus8.mb@gmail.com>
2025-09-04 00:43:05 +0200

    feat: post done

--- src/main/java/dev/jesus/project_spring_support_app/request/RequestController.java
+++ src/main/java/dev/jesus/project_spring_support_app/request/RequestController.java
@@ -12,6 +12,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(path = "${api-endpoint}/requests")
@@ -33,6 +35,28 @@ public class RequestController {
     RequestDTOResponse request = service.getEntityById(id);
 
     return ResponseEntity.ok().body(request);
+  }
+
+  @PostMapping("")
+  public ResponseEntity<RequestDTOResponse> storeEntity(@RequestBody RequestDTORequest dtoRequest) {
+
+    if (dtoRequest.description().isBlank())
+      return ResponseEntity.badRequest().build();
+
+    if (dtoRequest.user() == null) {
+      return ResponseEntity.badRequest().build();
+    }
+
+    if (dtoRequest.topic() == null) {
+      return ResponseEntity.badRequest().build();
+    }
+
+    RequestDTOResponse entityStored = service.storeEntity(dtoRequest);
+
+    if (entityStored == null)
+      return ResponseEntity.noContent().build();
+
+    return ResponseEntity.status(201).body(entityStored);
   }
 
 }
