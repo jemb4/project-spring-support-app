e7fbf7eef72040409578af2f615d3902a8132f6c
Jesus Martin <jesus8.mb@gmail.com>
2025-09-05 11:13:10 +0200

    feat: home redirect to swagger

---
+++ src/test/java/dev/jesus/project_spring_support_app/user/UserServiceImplTest.java
@@ -0,0 +1,8 @@
+package dev.jesus.project_spring_support_app.user;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+}
